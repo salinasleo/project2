3c093b512a3d07d5e46ab7cc25cbb0dcd5497196
Nick Iantosca <niantosca805@gmail.com>
2018-08-28 18:22:11 -0400

    working search route

--- public/assets/js/mentee.js
+++ public/assets/js/mentee.js
@@ -2,13 +2,13 @@ var occupation = [];
 $(document).ready(function() {
     
     $("#menteeSubmit").on("click", function(event) {
-      event.preventDefault();
+      // event.preventDefault();
 
-      console.log("Mentee registration form submitted");
-      console.log("Email input is " + document.getElementById("email").value);
-      console.log("First Name is " + document.getElementById("firstName").value);
-      console.log("Last Name is " + document.getElementById("lastName").value);
-      console.log("Zip Code is " + document.getElementById("zipCode").value);
+      // console.log("Mentee registration form submitted");
+      // console.log("Email input is " + document.getElementById("email").value);
+      // console.log("First Name is " + document.getElementById("firstName").value);
+      // console.log("Last Name is " + document.getElementById("lastName").value);
+      // console.log("Zip Code is " + document.getElementById("zipCode").value);
     //   console.log("Occupations interested in are " + document.getElementById("occupations").value);
     //   console.log("Occupations # 1 interested in are " + document.getElementById("occupations")[0].value);
     //   console.log("Occupations # 2 interested in are " + document.getElementById("occupations")[1].value);
@@ -17,7 +17,7 @@ $(document).ready(function() {
         console.log("Occupation " + (i+1) + " interested in is " + document.getElementById("occupations").selectedOptions[i].value);
         occupation[i] = document.getElementById("occupations").selectedOptions[i].value;
       }
-      console.log("Objective from program" + document.getElementById("objectives").value);
+      // console.log("Objective from program" + document.getElementById("objectives").value);
       // make a mentor obj
  var newMentee = {
   firstName: document.getElementById("firstName").value,
@@ -32,15 +32,15 @@ $(document).ready(function() {
   userType: "Mentee"
 };
 
-alert("Adding mentee...");
+// alert("Adding mentee...");
 
 // send an AJAX POST-request with jQuery
 $.post("/api/newmentee", newMentee)
   // on success, run this callback
   .then(function(data) {
     // log the data we found
-    console.log(data);
-    // res.render("search");
+    // console.log(data);
+     res.render("search");
     // tell the user we're adding a character with an alert window
    
   });
